08e71e8a38deef4dea435cd3f3b7156c395992c6
seeratawan01 <seeratsdsking@gmail.com>
2021-10-15 21:58:49 +0500

    Revert "Upload Files"
    This reverts commit 5c6438a5f7391187cd604285502c4f10be50ea68.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './scss/styles.scss';
 
 // import UserList class
-import { bubbleGantt as defaultExport } from './lib/bubbleGantt';
+import { myPlugin as defaultExport } from './lib/myPlugin';
 
 // export default UserList class
 // I used `defaultExport` to state that variable name doesn't matter
